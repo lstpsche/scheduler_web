b63a0458b17c2f5a48a9a4d4cf423ea085adfc1a
Nikita Silivonchik <lstpsche@gmail.com>
2019-09-22 16:06:24 +0300

    Add access methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   store_accessor :context, :last_message, :replace_last_message, :return_to
   has_many :schedule_users
   has_many :schedules, through: :schedule_users
+
+  alias :replace_last_message? :replace_last_message
+
+  def last_message_id
+    last_message[:result][:message_id]
+  end
 end
