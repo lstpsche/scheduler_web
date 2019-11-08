2bdb4f28d40f72d450f00cb22c2a66e8ee3496b1
Nikita Silivonchik <lstpsche@gmail.com>
2019-11-08 05:01:41 +0300

    #SR-31 fix all reek issues

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,8 +28,8 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
+rescue ActiveRecord::PendingMigrationError => error
+  puts error.to_s.strip
   exit 1
 end
 RSpec.configure do |config|
