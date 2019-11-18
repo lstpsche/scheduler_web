aa923b91ae14e44ed75bc5df88875f46eb20b263
Nikita Silivonchik <lstpsche@gmail.com>
2019-11-18 23:19:06 +0300

    #SR-54 create method for finding or creating user with params

--- app/helpers/bot_login_helper.rb
+++ app/helpers/bot_login_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module BotLoginHelper
+  def find_or_create_user(user_params)
+    User.find_by(id: user_params[:id]).presence || create_user_with(user_params)
+  end
+
   def create_user_with(user)
     User.create(
       id: user[:id],
