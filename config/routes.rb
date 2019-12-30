43598408950e5c605b5f44c2ec11bcfd6c94551a
Nikita Silivonchik <lstpsche@gmail.com>
2019-12-30 20:39:16 +0300

    #SR-117: comment out user profile edit page, show 404 at user_edit page, remove redundant devise routes, remove redundant passwords#edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'devise_custom/registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  devise_for :users, only: %i[sessions registrations], controllers: { registrations: 'devise_custom/registrations' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :schedules, except: %i[new edit] do
     resources :events, only: %i[create destroy]
