dcac00a96f989fa435c24f080d8742c9fd55cd84
Nikita Silivonchik <lstpsche@gmail.com>
2019-12-30 21:07:44 +0300

    #SR-153: add admin column to User, add admin dropdown to navbar for admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_23_195105) do
+ActiveRecord::Schema.define(version: 2019_12_30_175724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2019_11_23_195105) do
     t.string "bot_request_auth_token"
     t.boolean "global_bot_first_start", default: true, null: false
     t.boolean "students_bot_first_start", default: true, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
