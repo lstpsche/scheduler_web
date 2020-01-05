35c55be5191a2da9be8997c1b8dad4ab2d867ba3
Nikita Silivonchik <lstpsche@gmail.com>
2020-01-06 00:31:49 +0300

    #SR-113 rubocop: fix line is too long issue at application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,10 @@ module ApplicationHelper
   def user_avatar(user: current_user, size: 25)
     return unless user&.avatar&.attached?
 
-    image_tag(user.avatar.variant(resize: "#{size}x#{size}"), id: 'navbar-user-avatar', class: 'rounded-circle user-avatar')
+    image_tag(
+      user.avatar.variant(resize: "#{size}x#{size}"),
+      id: 'navbar-user-avatar',
+      class: 'rounded-circle user-avatar'
+    )
   end
 end
