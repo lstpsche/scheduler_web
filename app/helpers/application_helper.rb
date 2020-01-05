8e9bead685a518ec8208e2e77b7fd0c74cdc9091
Nikita Silivonchik <lstpsche@gmail.com>
2020-01-06 01:00:05 +0300

    #SR-186 refactor application controller and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,22 +3,8 @@
 module ApplicationHelper
   private
 
-  def render_404_error
-    render file: "#{Rails.root}/public/404", layout: false, status: :not_found
-  end
-
   def controller_action
     controller_name + '#' + action_name
-  end
-
-  def current_path
-    Rails.application.routes.recognize_path(request.url)
-  end
-
-  def clear_url_params
-    return unless request.query_parameters.present?
-
-    redirect_to current_path
   end
 
   def user_avatar(user: current_user, size: 25)
