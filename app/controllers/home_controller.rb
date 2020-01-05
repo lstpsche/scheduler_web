bc7bc6e30d55c09ecae26e00b1d2e14b49df53cd
Nikita Silivonchik <lstpsche@gmail.com>
2020-01-06 01:00:55 +0300

    #SR-186 refactor home controller, remove redundant logic from navbar partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index
+  before_action :redirect_to_schedules, only: %i(index)
+
+  def index; end
+
+  private
+
+  def redirect_to_schedules
     redirect_to schedules_path if current_user
   end
 end
