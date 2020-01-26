c0303ced7065c3e9a1cb568ab5eb874463761008
Nikita Silivonchik <lstpsche@gmail.com>
2020-01-26 13:14:44 +0300

    #SR-213: delete flash[:notice] for already_signed_out and flash[:alert] for already_authenticated

--- app/controllers/devise_custom/sessions_controller.rb
+++ app/controllers/devise_custom/sessions_controller.rb
@@ -7,5 +7,23 @@ module DeviseCustom
       super
       flash.delete(:notice)
     end
+
+    private
+
+    # Check if there is no signed in user before doing the sign out.
+    #
+    # If there is no signed in user, it will set the flash message and redirect
+    # to the after_sign_out path.
+    def verify_signed_out_user
+      super
+      flash.delete(:notice)
+    end
+
+    # Used in before_action for [:new, :create]
+    # Sets 'already_authenticated' alert if user is already authenticated
+    def require_no_authentication
+      super
+      flash.delete(:alert)
+    end
   end
 end
